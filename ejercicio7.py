294617564b20a07824c5dc7feed4393a26df799e
Santo1215 <132920526+Santo1215@users.noreply.github.com>
2023-07-18 20:30:58 -0500

    ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -5,61 +5,60 @@ class Vehiculo:
         self.posicion = posicion
         self.velocidad = velocidad
 
-    def move(self):
+    def mover(self):
         self.posicion += self.velocidad
 
-class TrafficLight:
+class Semaforo:
     def __init__(self, posicion):
         self.posicion = posicion
         self.color = "rojo"
 
-    def change_color(self):
+    def cambiar_color(self):
         self.color = "verde" if self.color == "rojo" else "rojo"
 
-class TrafficSimulator:
-    def __init__(self, largo, n_cars, traffic_lights):
+class Simuladortrafico:
+    def __init__(self, largo, num_carros, semaforos):
         self.largo = largo
-        self.n_cars = n_cars
-        self.traffic_lights = traffic_lights
+        self.num_carros = num_carros
+        self.semaforos = semaforos
         self.Vehiculos = []
-        for i in range(n_cars):
+        for i in range(num_carros):
             posicion = random.randint(0, largo)
             velocidad = random.randint(1, 5)
             self.Vehiculos.append(Vehiculo(posicion, velocidad))
 
-    def step(self):
+    def paso(self):
         for Vehiculo in self.Vehiculos:
-            Vehiculo.move()
-            for traffic_light in self.traffic_lights:
-                if Vehiculo.posicion == traffic_light.posicion and traffic_light.color == "rojo":
+            Vehiculo.mover()
+            for semaforo in self.semaforos:
+                if Vehiculo.posicion == semaforo.posicion and semaforo.color == "rojo":
                     Vehiculo.velocidad = 0
-                elif Vehiculo.posicion == traffic_light.posicion and traffic_light.color == "verde":
+                elif Vehiculo.posicion == semaforo.posicion and semaforo.color == "verde":
                     Vehiculo.velocidad = random.randint(1, 5)
-        for traffic_light in self.traffic_lights:
-            traffic_light.change_color()
+        for semaforo in self.semaforos:
+            semaforo.cambiar_color()
 
-    def run(self, steps):
-        for i in range(steps):
-            self.step()
+    def run(self, pasos):
+        for i in range(pasos):
+            self.paso()
             print(self)
 
     def __str__(self):
-        road = ["."] * self.largo
+        carretera = ["."] * self.largo
         for Vehiculo in self.Vehiculos:
             if Vehiculo.posicion >= self.largo:
                 continue
-            if road[Vehiculo.posicion] == ".":
-                road[Vehiculo.posicion] = "🚗"
+            if carretera[Vehiculo.posicion] == ".":
+                carretera[Vehiculo.posicion] = "🚗"
             else:
-                road[Vehiculo.posicion] += "🚗"
-        for traffic_light in self.traffic_lights:
-            if road[traffic_light.posicion] == ".":
-                road[traffic_light.posicion] = "🔴" if traffic_light.color == "rojo" else "🟢"
+                carretera[Vehiculo.posicion] += "🚗"
+        for semaforo in self.semaforos:
+            if carretera[semaforo.posicion] == ".":
+                carretera[semaforo.posicion] = "🔴" if semaforo.color == "rojo" else "🟢"
             else:
-                road[traffic_light.posicion] += "🔴" if traffic_light.color == "rojo" else "🟢"
-        return "".join(road)
+                carretera[semaforo.posicion] += "🔴" if semaforo.color == "rojo" else "🟢"
+        return "".join(carretera)
 
-# Ejemplo de uso
-traffic_lights = [TrafficLight(10), TrafficLight(20)]
-simulator = TrafficSimulator(50, 10, traffic_lights)
-simulator.run(20)
+semaforos = [Semaforo(10), Semaforo(20)]
+simulador = Simuladortrafico(50, 10, semaforos)
+simulador.run(20)
